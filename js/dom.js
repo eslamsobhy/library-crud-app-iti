c50c25fa2768df54e7697b944312a923851e8328
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-18 09:35:33 +0200

    handled user entering the number of books functionality

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,39 @@
 import { Book, Author } from "./classes.js";
 
-// TESTING
-const author = new Author("Ronald", "ronald@hehe.com");
-const book = new Book("The Hobbit", 120, author);
+// Variables
+var numOfBooks = 0;
 
-console.log(book);
+// DOM ELEMENTS
+// sections
+const numComp = document.querySelector(".num-container");
+const formComp = document.querySelector(".form-container");
+const displayComp = document.querySelector(".display-data");
+const editComp = document.querySelector(".edit");
+
+// inputs
+const numOfBooksInput = document.querySelector("#book-num");
+
+// buttons
+const numOk = document.querySelector(".num");
+
+// methods
+function confirmNumOfBooks() {
+  // When user enter the number of books
+  if (
+    !numOfBooksInput.value ||
+    !isFinite(numOfBooksInput.value) ||
+    numOfBooksInput.value == 0
+  ) {
+    numOfBooksInput.style.border = "2px solid red";
+  } else {
+    numOfBooksInput.style.border = "none";
+    numOfBooks = numOfBooksInput.value;
+    numComp.style.display = "none";
+    formComp.style.display = "flex";
+  }
+}
+
+// event listeners
+numOk.addEventListener("click", function () {
+  confirmNumOfBooks();
+});
