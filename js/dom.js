eea9634eca241145d46aad5a509716e146980ab7
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-18 14:37:16 +0200

    implemented some DOM manipulation to validate adding a book

--- js/dom.js
+++ js/dom.js
@@ -12,9 +12,17 @@ const editComp = document.querySelector(".edit");
 
 // inputs
 const numOfBooksInput = document.querySelector("#book-num");
+const bookNameInput = document.querySelector("#book-name");
+const bookPriceInput = document.querySelector("#book-price");
+const authorNameInput = document.querySelector("#author-name");
+const authorEmailInput = document.querySelector("#author-email");
 
 // buttons
-const numOk = document.querySelector(".num");
+const numOkBtn = document.querySelector(".num");
+const addBookBtn = document.querySelector(".add-book");
+
+// Initializations
+numOfBooksInput.focus();
 
 // methods
 function confirmNumOfBooks() {
@@ -24,16 +32,54 @@ function confirmNumOfBooks() {
     !isFinite(numOfBooksInput.value) ||
     numOfBooksInput.value == 0
   ) {
-    numOfBooksInput.style.border = "2px solid red";
+    numOfBooksInput.style.outline = "2px solid red";
+    numOfBooksInput.focus();
   } else {
-    numOfBooksInput.style.border = "none";
+    numOfBooksInput.style.outline = "none";
     numOfBooks = numOfBooksInput.value;
     numComp.style.display = "none";
     formComp.style.display = "flex";
   }
+  bookNameInput.focus();
+}
+
+function addBook() {
+  // When the user add a book
+  if (
+    !/^[A-Za-z]+([A-Za-z]+)?\s[A-Za-z]+([A-Za-z]+)?$/.test(bookNameInput.value)
+  ) {
+    bookNameInput.style.outline = "2px solid red";
+    bookNameInput.focus();
+  } else if (!/^\d*\.?\d+$/.test(bookPriceInput.value)) {
+    bookNameInput.style.outline = "none";
+    bookPriceInput.style.outline = "2px solid red";
+    bookPriceInput.focus();
+  } else if (
+    !/^[A-Za-z]+([A-Za-z]+)?\s[A-Za-z]+([A-Za-z]+)?$/.test(
+      authorNameInput.value
+    )
+  ) {
+    bookPriceInput.style.outline = "none";
+    authorNameInput.style.outline = "2px solid red";
+    authorNameInput.focus();
+  } else if (
+    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+      authorEmailInput.value
+    )
+  ) {
+    authorNameInput.style.outline = "none";
+    authorEmailInput.style.outline = "2px solid red";
+    authorEmailInput.focus();
+  } else {
+    authorEmailInput.style.outline = "none";
+  }
 }
 
 // event listeners
-numOk.addEventListener("click", function () {
+numOkBtn.addEventListener("click", function () {
   confirmNumOfBooks();
+});
+
+addBookBtn.addEventListener("click", function () {
+  addBook();
 });
